3cdeb2546a15c747cc562192e24d91df7f48b63f
mohamed-megahed1 <66644067+mohamed-megahed1@users.noreply.github.com>
2024-07-04 17:49:00 +0300

    49. Group Anagrams on leetcode

---
+++ Group Anagrams/src/Main.java
@@ -0,0 +1,41 @@
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        String [] strs = {"eat","tea","tan","ate","nat","bat"};
+
+
+    
+
+
+
+
+
+        System.out.println(groupAnagrams(strs));
+
+
+    }
+    public static List<List<String>> groupAnagrams(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return new ArrayList<>();
+        }
+
+        Map<String, List<String>> map = new HashMap<>();
+        for (String str : strs) {
+            char[] chars = str.toCharArray();
+            Arrays.sort(chars);
+            String sortedStr = new String(chars);
+
+            if (!map.containsKey(sortedStr)) {
+                map.put(sortedStr, new ArrayList<>());
+            }
+            map.get(sortedStr).add(str);
+        }
+
+        return new ArrayList<>(map.values());
+    }
+
+}
